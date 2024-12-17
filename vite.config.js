b1d78211163fb2e95b59b3d7be021227382bdf21
yaj55billy <yaj55ushydna@gmail.com>
2024-12-17 19:46:43 +0800

    adjust deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/react-back-youself/', 
+  base: import.meta.env.VITE_BASE_PATH || '/', // 從環境變數中讀取 BASE_PATH
   plugins: [react()],
-})
+});
