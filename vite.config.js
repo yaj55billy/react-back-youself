110301a1a0bedd956e5342049e7b0893ab429ff8
yaj55billy <yaj55ushydna@gmail.com>
2024-12-17 20:47:01 +0800

    adjust deploy config..

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const basePath = mode === 'development' ? '/' : (import.meta.env.VITE_BASE_PATH || '/');
+export default defineConfig(() => {
 
   return {
-    base: basePath, 
+    base: /react-back-youself/, 
     plugins: [react()],
   };
 });
