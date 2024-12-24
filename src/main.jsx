a51b6be7c10b697f0f0609edb6435e7cea4a1c36
yaj55billy <yaj55ushydna@gmail.com>
2024-12-24 19:54:03 +0800

    fix: clear deploy set(think about router)

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-
-const basename = "/react-back-youself/week4/";
 
 createRoot(document.getElementById("root")).render(
 	// <StrictMode>
-	<BrowserRouter basename={basename ? basename : "/"}>
-		<App />
-	</BrowserRouter>
+	<App />
 	// </StrictMode>,
 );
