c8b9406528d34b388a2912f3bf4c1985846a70f6
yaj55billy <yaj55ushydna@gmail.com>
2024-12-24 20:09:05 +0800

    fix: adjust BrowserRouter and config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
 	return {
-		base: "/react-back-youself/",
+		base: "/", //react-back-youself-week4
 		plugins: [react()],
 	};
 });
