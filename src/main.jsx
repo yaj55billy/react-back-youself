6127c5ebc2399c260c96416a61d299b56521a34a
yaj55billy <yaj55ushydna@gmail.com>
2024-12-24 17:13:26 +0800

    adjust BrowserRouter basename

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+const basename = import.meta.env.VITE_BASE_PATH || "/";
+
 createRoot(document.getElementById("root")).render(
 	// <StrictMode>
-	<BrowserRouter>
+	<BrowserRouter basename={basename}>
 		<App />
 	</BrowserRouter>
 	// </StrictMode>,
