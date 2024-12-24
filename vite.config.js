fa4eb4c58a6f34ae6eff97699150620a7b68aad8
yaj55billy <yaj55ushydna@gmail.com>
2024-12-24 17:42:39 +0800

    adjust deploy config...test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
 	return {
-		base: "/react-back-youself/",
+		// base: "/react-back-youself/",
+		base: import.meta.env.VITE_BASE_PATH || "/",
 		plugins: [react()],
 	};
 });
