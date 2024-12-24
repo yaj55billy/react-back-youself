da28d63554a96142e3fa579f6cae6137e7c4464c
yaj55billy <yaj55ushydna@gmail.com>
2024-12-24 16:41:45 +0800

    test path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-	let base = "/";
-	if (mode === "development") {
-		base = "/react-back-youself/";
-	}
+export default defineConfig(() => {
+	// let base = "/";
+	// if (mode === "development") {
+	// 	base = "/react-back-youself/";
+	// }
 
 	return {
-		base, // react-back-youself/
+		base: "/react-back-yourself/",
 		plugins: [react()],
 	};
 });
